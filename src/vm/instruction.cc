c40469fce598f82c8800cee3b804ab7bdfcaf252
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-24 20:53:56 +0530

    Format of Instructions for vm

---
+++ src/vm/instruction.cc
@@ -0,0 +1,51 @@
+#include "vm/instruction.h"
+#include "common/exceptions.h"
+#include "object/jsobject.h"
+#include <string>
+#include <sstream>
+
+namespace grok {
+namespace vm {
+
+const char *instr_to_string[] = {
+#define INSTRUCTION_OP(instr) #instr
+    INSTRUCTION_LIST(INSTRUCTION_OP)
+#undef INSTRUCTION_OP
+};
+
+const char *type_to_string[] = {
+#define DATA_TYPE_OP(type) #type
+    DATA_TYPE_LIST(DATA_TYPE_OP)
+#undef DATA_TYPE_OP
+};
+
+/// InstrDataTypeToString ::= returns the string representation of the 
+/// instruction embedded data. It can be null, number or string only
+std::string InstrDataToString(const RawInstruction<int> &instr)
+{
+    switch (instr.GetDataType()) {
+    default:
+        throw std::runtime_error("fatal: unknown data type in the instruction");
+
+    case DataTypes::d_null:
+        return "Null";
+
+    case DataTypes::d_num:
+        return std::to_string(instr.GetNumber());
+
+    case DataTypes::d_str:
+        return instr.GetString();
+    }
+}
+
+std::string InstructionToString(const RawInstruction<int> &instr)
+{
+    std::ostringstream out;
+    out << std::left << std::setw(8) << instr_to_string[instr.kind_];
+    out << std::setw(6) << type_to_string[instr.data_type_];
+    out << InstrDataToString(instr);
+    return out.str();
+}
+
+}   // vm
+}   // grok
---
+++ src/vm/instruction.h
@@ -0,0 +1,97 @@
+#ifndef INSTRUCTION_H_
+#define INSTRUCTION_H_
+
+#include "object/object.h"
+#include <cctype>
+#include <vector>
+
+namespace grok {
+namespace vm {
+
+#define INSTRUCTION_LIST(op)    \
+    op(noop),   \
+    op(fetch),  \
+    op(store),  \
+    op(push),   \
+    op(pushim), \
+    op(lts),     \
+    op(gts),     \
+    op(eqs),     \
+    op(neqs),    \
+    op(adds),   \
+    op(subs),   \
+    op(muls),   \
+    op(divs),   \
+    op(shls),   \
+    op(shrs),   \
+    op(rems),   \
+    op(loopz),  \
+    op(jmp),    \
+    op(call),   \
+    op(ret),    \
+    op(jmpz)
+
+#define DATA_TYPE_LIST(op)  \
+    op(d_null), \
+    op(d_bool), \
+    op(d_num),  \
+    op(d_str),  \
+    op(d_name)
+
+enum Instructions {
+#define INSTRUCTION_OP(instr) instr
+INSTRUCTION_LIST(INSTRUCTION_OP)
+#undef INSTRUCTION_OP
+};
+
+enum Datatypes {
+#define DATA_TYPE_OP(type) type
+    DATA_TYPE_LIST(DATA_TYPE_OP)
+#undef DATA_TYPE_OP
+};
+
+/// RawInstruction ::= template data structure for instruction
+///     @param T ::= represents the type of address for jmp instructions
+template <T>
+struct RawInstruction {
+    int32_t kind_;
+    int32_t data_type_;
+    bool boolean_;
+    double number_;
+    std::string str_;
+    T jmp_addr_;
+    grok::obj::Object data_;    // will be used in future
+
+    auto GetKind() const { return kind_; }
+    auto GetDataType() const { return data_type_; }
+    auto GetData() const { return data_; }
+    auto GetString() const { return str_; }
+    auto GetNumber() const { return number_; }
+
+    bool IsKindOfJump() const
+    {
+        switch (static_cast<Instructions>(kind_)) {
+        default:
+            return false;
+        case jmp:
+        case jmpz:
+        case loopz:
+            return true;
+        }
+    }
+
+    void SetJumpLength(T sz)
+    {
+        jmp_addr_ = sz;
+    }
+};
+
+extern const char *instr_to_string[];
+extern const char *type_to_string[];
+
+extern std::string InstrDataToString(const RawInstruction<int> &instr);
+extern std::string InstructionToString(const RawInstruction<int> &instr);
+} // vm
+} // grok
+
+#endif
