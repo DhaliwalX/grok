d8cf25930dbc738b8f7b1d031b717e9ef5db0c20
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-25 18:07:14 +0530

    Code generation for functions

---
+++ src/object/function.cc
@@ -0,0 +1,48 @@
+#include "object/function.h"
+#include "vm/codegen.h"
+
+namespace grok {
+namespace obj {
+using namespace grok::vm;
+using namespace grok::parser;
+
+bool Function::HasCodeGened() const 
+{
+    return Native || CodeGened;
+}
+
+void Function::PrepareFunction()
+{
+    if (HasCodeGened())
+        return;
+
+    if (!AST)
+        throw std::runtime_error("there was no function prototype");
+
+    CodeGenerator Generator;
+    Generator.Generate(AST.get());
+    IR = Generator.GetIR();
+    CodeGened = true;
+}
+
+bool Function::IsNative() const 
+{
+    return Native;
+}
+
+Counter Function::GetAddress()
+{
+    // instead of returning only beginning we could have returned end
+    // also so to avoid iterator overflow, but during code generation
+    // stores a leave instruction at the end. So we don't have to worry
+    // about that.
+    return IR->begin();
+}
+
+const std::vector<std::string> &Function::GetArgs() const
+{
+    return Proto->GetArgs();
+}
+
+}
+}
--- src/vm/genfunction.cc
+++ src/vm/genfunction.cc
@@ -1,20 +1,35 @@
+#include "object/object.h"
+#include "object/jsobject.h"
+#include "object/function.h"
 #include "parser/functionstatement.h"
 #include "vm/instruction-builder.h"
 #include "vm/instruction.h"
+#include "vm/context.h"
+#include "vm/vm.h"
 
 namespace grok {
 namespace parser {
 
 using namespace grok::vm;
-/// emits code for function
-void FunctionStatement::emit(std::shared_ptr<InstructionBuilder> builder)
-{
+using namespace grok::obj;
 
+/// emits code for function definition
+/// We don't generate the code for the body now but defer it until
+/// it is required i.e. when a call is placed to this function during
+/// execution
+void FunctionStatement::emit(std::shared_ptr<InstructionBuilder>)
+{
+    std::string Name = proto_->GetName();
+    auto F = std::make_shared<Function>(std::move(body_), std::move(proto_));
+    auto VS = GetVStore(GetGlobalVMContext());
+    auto V = Value{};
+    V.O = std::make_shared<Object>(F);
+    VS->StoreValue(Name, V);
 }
 
-void FunctionPrototype::emit(std::shared_ptr<InstructionBuilder> builder)
+void FunctionPrototype::emit(std::shared_ptr<InstructionBuilder>)
 {
-    
+    // do nothing
 }
 
 } // parser
