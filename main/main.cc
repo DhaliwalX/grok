1c6e6c6774147f29722b2b4d4564458d6ee9d77c
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-25 18:07:33 +0530

    Updated main.cc

--- main/main.cc
+++ main/main.cc
@@ -1,32 +1,8 @@
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//    +-----------------------------------------------------------+
-//    |          Grok : A Naive JavaScript Interpreter            |
-//    +-----------------------------------------------------------+
-//
-// Copyright 2015 Pushpinder Singh
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-//
-//  \file:  main.cc
-//  \description:  definition of main
-//  \author: Pushpinder Singh
-//
-///////////////////////////////////////////////////////////////////////////////
-
 #include "parser/parser.h"
+#include "vm/context.h"
+#include "vm/printer.h"
+#include "vm/codegen.h"
+#include "vm/instruction-list.h"
 #include <iostream>
 #include <memory>
 #include <string>
@@ -34,7 +10,10 @@
 int main(int argc, char *argv[]) {
     std::string in;
     bool result = false;
+    using namespace grok::vm;
+    using namespace grok::parser;
 
+    InitializeVMContext();
     std::cout << ">> ";
     std::getline(std::cin, in);
     auto lex = std::make_unique<Lexer>(in);
@@ -42,10 +21,18 @@ int main(int argc, char *argv[]) {
     grok::parser::GrokParser parser{ std::move(lex) };
     result = parser.ParseExpression();
 
-    if (result) {
-        std::cout << parser;
-    } else {
+    std::cout << std::string(75, '=') << std::endl;
+    if (!result) {
         std::cout << "Errors occurred" << std::endl;
+        return -1;
     }
+
+    auto AST = parser.ParsedAST();
+    CodeGenerator CG;
+    CG.Generate(AST.get());
+
+    auto IR = CG.GetIR();
+    Printer P{ std::cout };
+    P.Print(IR);
     return 0;
 }
