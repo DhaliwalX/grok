41d54cb2b89a54a3790d981839f4109677f2a5b3
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-27 14:55:23 +0530

    Scopes support added

--- src/vm/var-store.cc
+++ src/vm/var-store.cc
@@ -4,23 +4,49 @@ namespace grok {
 namespace vm {
 
 VStore::VStore()
-    : MV{}
-{ }
+    : MV{}, VS{}
+{
+    MV = std::make_shared<MappedValues>();
+}
 
 Value VStore::GetValue(const std::string &N)
 {
-    auto Result = MV.find(N);
+    auto Result = MV->find(N);
 
-    if (Result == MV.end())
-        throw std::runtime_error(std::string() 
-            + "variable '" + N + "' doesn't exists");
+    if (Result == MV->end()) {
+        return TryForOtherScopes(N);
+    }
 
     return Result->second;
 }
 
 void VStore::StoreValue(const std::string &N, Value V)
 {
-    MV[N] = V;
+    MV->insert({ N, V });
+}
+
+void VStore::CreateScope()
+{
+    VS.Push(MV);
+    MV = std::make_shared<MappedValues>();
+}
+
+void VStore::RemoveScope()
+{
+    MV = VS.Pop();
+}
+
+Value VStore::TryForOtherScopes(const std::string &Name)
+{
+    for (auto i = VS.rbegin(); i != VS.rend(); i++) {
+        auto Result = (*i)->find(Name);
+
+        if (Result != (*i)->end())
+            return Result->second;
+    }
+
+    throw std::runtime_error(std::string() + "no variable named '"
+        + Name + "'");
 }
 
 } // vm
--- src/vm/var-store.h
+++ src/vm/var-store.h
@@ -2,8 +2,10 @@
 #define VAR_STORE_H_
 
 #include "object/object.h"
+#include "common/generic-stack.h"
 #include <map>
 #include <string>
+#include <vector>
 
 namespace grok {
 namespace vm {
@@ -11,16 +13,28 @@ namespace vm {
 /// Value ::= Value stored in map
 struct Value {
     std::shared_ptr<grok::obj::Object> O;
+    std::shared_ptr<std::string> S;
 
-    Value(std::shared_ptr<grok::obj::Object> O)
-        : O(O)  // A man with a Glass
+    Value(std::shared_ptr<grok::obj::Object> O,
+        std::string S = "")
+        : O{ O }, S{ std::make_shared<std::string>(S) }
     { }
 
     Value() = default;
 };
 
+template <typename T>
+static decltype(auto) GetObjectPointer(Value V)
+{
+    auto obj = V.O;
+    auto pointer = obj->as<T>();
+    return pointer;
+}
+
 /// MappedValues ::= value mapped to string
 using MappedValues = std::map<std::string, Value>;
+using MappedValuesHandle = std::shared_ptr<MappedValues>;
+using VStoreInternalStack = GenericStack<MappedValuesHandle>;
 
 /// VStore ::= class for storing the variables and functions
 /// where each of these variables can be accessed using there names
@@ -33,8 +47,17 @@ public:
 
     /// StoreValue ::= stores the value from the string
     void StoreValue(const std::string &N, Value V);
+
+    /// CreateScope ::= start a new scope
+    void CreateScope();
+
+    /// RemoveScope ::= remove the scope and associated variables
+    void RemoveScope();
+
+    Value TryForOtherScopes(const std::string &N);
 private:
-    MappedValues MV;
+    MappedValuesHandle MV;
+    VStoreInternalStack VS;
 };
 
 } // vm
