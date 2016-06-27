cb02720448d81a87da5328de7574da4840e26a04
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-27 14:53:51 +0530

    TODO: Code must be generated in reverse direction

--- src/vm/genarray.cc
+++ src/vm/genarray.cc
@@ -11,6 +11,7 @@ void ArrayLiteral::emit(std::shared_ptr<InstructionBuilder> builder)
 {
     // we generate instructions for the expressions, the result
     // of all those instructions will be stored in stack
+    // TODO: Code generation will be done in reverse
     for (auto &i : exprs_) {
         i->emit(builder);
     }
@@ -19,12 +20,13 @@ void ArrayLiteral::emit(std::shared_ptr<InstructionBuilder> builder)
 
     // TODO : find a better way to store the size
     instr->number_ = static_cast<double>(reserve);
-    instr->data_type_ = d_null;
+    instr->data_type_ = d_num;
     builder->AddInstruction(std::move(instr));
 
     // copy the values stored in stack to the reserved array
     instr = InstructionBuilder::Create<Instructions::cpya>();
-    instr->data_type_ = d_null;
+    instr->data_type_ = d_num;
+    instr->number_ = static_cast<double>(reserve);
     builder->AddInstruction(std::move(instr));
 
     // push the array so created to the stack
