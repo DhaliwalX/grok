d0b7519e20262636d31cbe536fb1c43e1f64a938
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-27 14:48:53 +0530

    Added simple stack code

---
+++ src/common/generic-stack.h
@@ -0,0 +1,39 @@
+#ifndef GENERIC_STACK_H_
+#define GENERIC_STACK_H_
+
+#include <vector>
+
+/// Stack ::= used for running interpreter for storing objects
+template <typename T>
+class GenericStack : public std::vector<T> {
+public:
+    using base = std::vector<T>;
+
+    GenericStack()
+        : std::vector<T>{}
+    { }
+
+    void Push(T object)
+    {
+        base::push_back(object);
+    }
+    
+    T Pop()
+    {
+        auto V = base::back();
+        base::pop_back();
+        return V;
+    }
+
+    T Top()
+    {
+        return base::back();
+    }
+
+    bool Empty()
+    {
+        return base::empty();
+    }
+};
+
+#endif
