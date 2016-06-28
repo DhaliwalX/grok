5e4d4e0bcf213bc8978e3ff6f6c91ec527c5e2d5
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-06-28 08:28:13 +0530

    AST for return statement

---
+++ src/parser/returnstatement.h
@@ -0,0 +1,28 @@
+#ifndef RETURN_STATEMENT_H_
+#define RETURN_STATEMENT_H_
+
+#include "parser/expression.h"
+
+namespace grok {
+namespace parser {
+
+class ReturnStatement : public Expression {
+public:
+    ReturnStatement(std::unique_ptr<Expression> expr)
+        : expr_{ std::move(expr) }
+    { }
+
+    std::ostream &operator<<(std::ostream &os) const override
+    {
+        os << "(return ";
+        return *expr_ << os << ")";
+    }
+
+    void emit(std::shared_ptr<grok::vm::InstructionBuilder>) override;
+private:
+    std::unique_ptr<Expression> expr_;
+};
+
+} // parser
+} // grok
+#endif
