a06e619a667b1d5c274eac5c14531d7ef4425aba
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-07-13 11:57:57 +0530

    Removed unneccessary dependencies for faster compilation

--- src/vm/genfunction.cc
+++ src/vm/genfunction.cc
@@ -1,11 +1,7 @@
-#include "object/object.h"
-#include "object/jsobject.h"
 #include "object/function.h"
 #include "parser/functionstatement.h"
 #include "vm/instruction-builder.h"
 #include "vm/instruction.h"
-#include "vm/context.h"
-#include "vm/vm.h"
 
 namespace grok {
 namespace parser {
@@ -21,18 +17,26 @@ void FunctionStatement::emit(std::shared_ptr<InstructionBuilder> builder)
 {
     std::string Name = proto_->GetName();
     auto F = CreateFunction(std::move(body_), std::move(proto_));
-    auto VS = GetVStore(GetGlobalVMContext()); 
-
-    if (Name.length()) {
-        auto V = Value{};
-        V.O = (F);
-        VS->StoreValue(Name, V);
-    }
 
     auto instr = InstructionBuilder::Create<Instructions::push>();
     instr->data_type_ = d_obj;
     instr->data_ = (F);
 
+    builder->AddInstruction(std::move(instr));
+    instr = InstructionBuilder::Create<Instructions::news>();
+    instr->data_type_ = d_name;
+    instr->str_ = Name;
+    builder->AddInstruction(std::move(instr));
+    
+    instr = InstructionBuilder::Create<Instructions::fetch>();
+    instr->data_type_ = d_name;
+    instr->str_ = Name;
+    builder->AddInstruction(std::move(instr));
+
+    instr = InstructionBuilder::Create<Instructions::pushim>();
+    builder->AddInstruction(std::move(instr));
+
+    instr = InstructionBuilder::Create<Instructions::store>();
     builder->AddInstruction(std::move(instr));
 }
 
