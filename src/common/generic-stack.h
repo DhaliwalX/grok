a0210b246a31aee1096a2eb2b5ef908c3a623f42
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-07-19 13:48:35 +0530

    Don't inherit from std::vector<T>

--- src/common/generic-stack.h
+++ src/common/generic-stack.h
@@ -6,17 +6,18 @@
 
 /// Stack ::= used for running interpreter for storing objects
 template <typename T>
-class GenericStack : public std::vector<T> {
+class GenericStack {
 public:
     using base = std::vector<T>;
+    using size_type = typename base::size_type;
 
     GenericStack()
-        : std::vector<T>{}
+        : container_{}
     { }
 
     void Push(T object)
     {
-        base::push_back(object);
+        container_.push_back(object);
     }
     
     T Pop()
@@ -24,8 +25,8 @@ public:
         if (Empty()) {
             throw std::runtime_error("fatal: stack was empty");
         }
-        auto V = base::back();
-        base::pop_back();
+        auto V = container_.back();
+        container_.pop_back();
         return V;
     }
 
@@ -34,13 +35,71 @@ public:
         if (Empty()) {
             throw std::runtime_error("fatal: stack was empty");
         }
-        return base::back();
+        return container_.back();
     }
 
-    bool Empty()
+    bool Empty() const
     {
-        return base::empty();
+        return container_.empty();
     }
+
+    ~GenericStack()
+    {
+    }
+
+    T &operator[](size_t idx)
+    {
+        return container_[idx];
+    }
+
+    void push_back(const T &t)
+    {
+        container_.push_back(t);
+    }
+
+    typename base::reverse_iterator rbegin()
+    {
+        return container_.rbegin();
+    }
+
+    typename base::reverse_iterator rend()
+    {
+        return container_.rend();
+    }
+
+
+    typename base::iterator begin()
+    {
+        return container_.begin();
+    }
+
+
+    typename base::iterator end()
+    {
+        return container_.end();
+    }
+
+    void clear()
+    {
+        container_.clear();
+    }
+
+    bool empty() const 
+    {
+        return Empty();
+    }
+
+    size_type size() const
+    {
+        return container_.size();
+    }
+
+    void resize(size_type sz)
+    {
+        container_.resize(sz);
+    }
+private:
+    base container_;
 };
 
 #endif
