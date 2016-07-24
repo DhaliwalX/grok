cb31e464d731cf3a77f540972d288020626d10b8
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-07-24 09:38:37 +0530

    Update test

--- test/testing/test048.js
+++ test/testing/test048.js
@@ -1,7 +1,51 @@
 // simple linked list
-var LinkedListNode = function (data) {this.data = data;this.next = null;};var LinkedList = function(){this.head = null;this.size = 0;};
+var LinkedListNode = function (data) {
+    this.data = data;
+    this.next = null;
+};
 
-LinkedList.prototype.push = function(data) { var tmp = this.head;this.head = new LinkedListNode(data);this.head.next = tmp;};
+var LinkedList = function() {
+    this.head = null;
+    this.size  = 0;
+};
+
+LinkedList.prototype.push = function(data) {
+    var tmp = this.head;
+    this.head = new LinkedListNode(data);
+    this.head.next = tmp;
+    this.size++;
+};
+
+LinkedList.prototype.find = function(key) {
+    var node = this.head;
+    var parent = node;
+
+    while (node != null) {
+        if (node.data == key)
+            return { node: node, parent: parent };
+        parent = node;
+        node = node.next;
+    }
+
+    return null;
+};
+
+LinkedList.prototype.remove = function (key) {
+    var res = this.find(key);
+    if (res == null)
+        return false;
+
+    if (res.node.data == res.parent.data) {
+        if (res.node.data == this.root.data) {
+            this.root = null;
+            this.size = 0;
+        }
+    }
+
+    res.parent.next = res.node.next;
+    this.size--;
+    return true;
+};
 
 LinkedList.prototype.forEach = function(func) {
     var node = this.head;
@@ -15,7 +59,13 @@ LinkedList.prototype.forEach = function(func) {
 var list = new LinkedList();
 list.push(10);
 list.push(20);
+list.push(15);
 list.push(30);
+list.push(40);
+list.remove(15);
 
-var x = 30;
-list.forEach(function(a) { assert_equal(x, a); x = x - 10; });
+var x = 40;
+list.forEach(function(a) {
+    assert_equal(x, a);
+    x = x - 10;
+});
